34806e343413c914508908c4ac597a9027fd334e
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-05 19:07:02 +0800

    Espresso

---
+++ src/main/java/com/rendyrobbani/espresso/database/Schema.java
@@ -0,0 +1,29 @@
+package com.rendyrobbani.espresso.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Schema {
+
+	String getName();
+
+	List<Table> getTables();
+
+	List<Constraint> getChecks();
+
+	List<Constraint> getForeignKeys();
+
+	List<Constraint> getUniqueKeys();
+
+	default String getDDL() {
+		var value = new ArrayList<String>();
+
+		if (!this.getTables().isEmpty()) value.add(String.join(System.lineSeparator(), this.getTables().stream().map(Table::getCreateDDL).toList()));
+		if (!this.getChecks().isEmpty()) value.add(String.join(System.lineSeparator(), this.getChecks().stream().map(Constraint::getCreateDDL).toList()));
+		if (!this.getForeignKeys().isEmpty()) value.add(String.join(System.lineSeparator(), this.getForeignKeys().stream().map(Constraint::getCreateDDL).toList()));
+		if (!this.getUniqueKeys().isEmpty()) value.add(String.join(System.lineSeparator(), this.getUniqueKeys().stream().map(Constraint::getCreateDDL).toList()));
+
+		return String.join(System.lineSeparator().repeat(2), value);
+	}
+
+}
